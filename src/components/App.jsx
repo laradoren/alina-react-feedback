6ae63f60ccf56e32480b6add7418d0868d2cf031
Alina Halushko <alinahalushko2@gmail.com>
2022-12-05 22:29:57 +0200

    used prettier for refactoring code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-import React from "react"
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { Statictics } from "./Statistics/Statictics";
-import { Notification } from "./Notification/Notification";
+import React from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statictics } from './Statistics/Statictics';
+import { Notification } from './Notification/Notification';
 export class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { good: 0, bad: 0, neutral: 0 };
-    this.countPositiveFeedbackPercentage = this.countPositiveFeedbackPercentage.bind(this);
+    this.countPositiveFeedbackPercentage =
+      this.countPositiveFeedbackPercentage.bind(this);
     this.countTotalFeedback = this.countTotalFeedback.bind(this);
     this.onLeaveFeedback = this.onLeaveFeedback.bind(this);
   }
@@ -16,25 +17,36 @@ export class App extends React.Component {
   }
 
   countPositiveFeedbackPercentage() {
-    return Math.round(this.state.good/(this.state.bad + this.state.good + this.state.neutral)*100);
+    return Math.round(
+      (this.state.good /
+        (this.state.bad + this.state.good + this.state.neutral)) *
+        100
+    );
   }
 
   onLeaveFeedback(opt) {
     this.setState(opt);
   }
 
-  render () {
-  return (
-    <div>
-      <FeedbackOptions options={this.state} onLeaveFeedback={this.onLeaveFeedback}  />
-      {this.countTotalFeedback() ? <Statictics
-        good={this.state.good}
-        bad={this.state.bad}
-        neutral={this.state.neutral}
-        total={this.countTotalFeedback()}
-        positivePercentage={this.countPositiveFeedbackPercentage()}
-        /> : <Notification message="There is no feedback" />}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <FeedbackOptions
+          options={this.state}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        {this.countTotalFeedback() ? (
+          <Statictics
+            good={this.state.good}
+            bad={this.state.bad}
+            neutral={this.state.neutral}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </div>
+    );
   }
-};
+}
